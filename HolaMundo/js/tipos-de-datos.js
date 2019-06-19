19b96b912ab3c3f88a1c50fc7d92a88c6d579b19
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-19 12:58:10 -0500

    Operadores y tipos de datos

---
+++ HolaMundo/js/tipos-de-datos.js
@@ -0,0 +1,23 @@
+'use strict';
+
+ // Operadores 
+//  +,-,/-*-%
+ var numero1 = 7;
+ var numero2 = 12;
+ var operacion = numero1 * numero2;
+
+ alert("El resultado de la operación es: "+operacion);
+
+ //tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola 'que' tal";
+var verdadero_falso = true; //false
+
+var numero_falso = "33";
+
+// funciones
+console.log(Number(numero_falso)+7);
+// para este mismo caso se puede usar parseInt, y parseFloat
+
+//para convertir un numero a una cadena de texto
+console.log(String(numero_entero)+4); //El resultado se 444
