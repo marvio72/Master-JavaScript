36631e111ffcaa0c301e674e2ef5a00216248d79
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-19 15:03:06 -0500

    Bucle While

---
+++ HolaMundo/js/bucle-while.js
@@ -0,0 +1,16 @@
+'use strict';
+// BUCLE WHILE
+var year = 2019;
+
+// Incremento
+while (year <= 2051) {
+    // ejecuta esto
+    console.log("Estamos en el año :"+year);
+    year++;
+}
+console.log("___________________________________________________");
+// Decremento
+while (year != 1991) {
+    console.log("Es el año: "+year);
+    year--;
+}
