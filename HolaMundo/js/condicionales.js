fa8ac4eb386b109055801efaf42161969fe42471
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-19 14:30:23 -0500

    OPERADORES LOGICOS

--- HolaMundo/js/condicionales.js
+++ HolaMundo/js/condicionales.js
@@ -4,7 +4,7 @@
 // Si A es igual(diferente, mayor, menor) a B entonces haz algo
 
 var edad1 = 30;
-var edad2 = 12
+var edad2 = 12;
 
 // Si pasa esto
 if (edad1 > edad2) {
@@ -39,3 +39,28 @@ if (edad >= 18) {
     }
 
 
+/* Operadores Lógicos
+AND(Y): &&
+OR(O): ||
+NEGACIÓN: !
+*/
+
+//NEGACIÓN
+var year = 2018;
+if (year != 2016) {
+    console.log("El año no es 2016 realmente es: "+year);
+}
+
+//AND
+if (year >= 2000 && year <= 2020) {
+    console.log('Estamos en la era actual');
+}else{
+    console.log('Estamos en la era pos Moderna');
+}
+
+//OR
+if (year == 2008 || (year >= 2018 && year == 2028)) {
+    console.log('El año acaba en 8');
+}else{
+    console.log('AÑO NO REGISTRADO');
+}
