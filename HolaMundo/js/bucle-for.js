bd2bdbc751ed9a50edc772867afe0935d5ae0714
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-19 14:52:30 -0500

    Bucle For

---
+++ HolaMundo/js/bucle-for.js
@@ -0,0 +1,10 @@
+'use strict';
+// BUCLE FOR 
+// BUCLE es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for (let i = 1; i <= numero; i++) {
+    console.log(i);
+    
+}    
