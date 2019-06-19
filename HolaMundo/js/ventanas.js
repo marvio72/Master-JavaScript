4a254cfccc96641b25fc3d8fd1cd7e5c0f7c330c
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-19 16:53:19 -0500

    Alertas y Popups para ingreso de datos

---
+++ HolaMundo/js/ventanas.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//ALERTA
+alert("Esta es mi alerta!!");
+
+
+//CONFIRMACIÓN
+var mi_resultado = confirm('¿Estas seguro de querer continuar?');
+console.log(mi_resultado);
+
+// INGRESO DATOS
+var mi_respuesta = prompt("¿Que edad tienes?" , 18);
+console.log(mi_respuesta);
