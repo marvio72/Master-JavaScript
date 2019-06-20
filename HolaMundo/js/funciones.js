63afbcc9069ba57367c39c9929b32fe1b036b7bc
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-20 18:50:45 -0500

    Parametros Opcionales

--- HolaMundo/js/funciones.js
+++ HolaMundo/js/funciones.js
@@ -3,15 +3,23 @@
 //parametros
 
 
-function calculadora(numero1,numero2){
-    console.log("Suma:           "+(numero1+numero2));
-    console.log("Resta:          "+(numero1-numero2));
-    console.log("Multiplicación: "+(numero1*numero2));
-    console.log("División:       "+(numero1/numero2));
-    console.log('**********************************');
+function calculadora(numero1,numero2,mostrar = false){
+    
+    if (mostrar == false) {
+        console.log("Suma:           " + (numero1 + numero2));
+        console.log("Resta:          " + (numero1 - numero2));
+        console.log("Multiplicación: " + (numero1 * numero2));
+        console.log("División:       " + (numero1 / numero2));
+        console.log('**********************************');
+    }else{
+        document.write("Suma:           "+(numero1+numero2)+'<br/>');
+        document.write("Resta:          "+(numero1-numero2)+'<br/>');
+        document.write("Multiplicación: "+(numero1*numero2)+'<br/>');
+        document.write("División:       "+(numero1/numero2)+'<br/>');
+        document.write('**********************************<br/>');
+        }
 }
 
 for (let i = 0; i <= 10; i++) {
-    console.log(i);
-    calculadora(i,10);
+    calculadora(i,10,true); //Aqui agregamos el parametro opcional
 }
