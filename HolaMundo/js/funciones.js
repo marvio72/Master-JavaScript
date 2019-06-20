6f36c04c21933eed526d390d1919598cdfeff325
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-20 16:30:53 -0500

    Funciones

---
+++ HolaMundo/js/funciones.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Funciones
+//Es una agrupación reutilizable de un conjunto de instrucciones
+
+//Defino la función
+function calculadora(){
+    //Conjunto de instrucciones a ejecutar
+    return "Hola soy la calculadora";
+}
+//Invocar o llamar a la función
+calculadora();
+//otra manera de invocarla es la siguiente
+var resultado = calculadora();
+console.log(resultado);
