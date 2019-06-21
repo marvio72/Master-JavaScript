71e300da8310620b3b8afa6bab17c137daddd109
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-20 22:18:04 -0500

    Funciones anonimas

---
+++ HolaMundo/js/funciones-anonimas.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//Funciones anonimas
+//Es una funcion que no tiene nombre
+
+var pelicula = function(nombre){
+    return "La pelicuas es: "+nombre; 
+};
+
+//Otro ejemplo
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
+    var sumar = numero1 + numero2;
+
+    sumaYmuestra(sumar);
+    sumaPorDos(sumar);
+
+    return sumar;
+}
+
+sumame(7,6,function(dato){
+    console.log("La suma es:" + dato);
+},
+function(dato){
+    console.log("La suma por dos es: " + dato*2);
+});
