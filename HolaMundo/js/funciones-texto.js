439ac8bc23301c5330645e0f64f423c83adaaf46
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-21 00:43:03 -0500

    Metodos para procesar textos

--- HolaMundo/js/funciones-texto.js
+++ HolaMundo/js/funciones-texto.js
@@ -2,29 +2,41 @@
 
 //Transformación de Textos
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript de Victor Robles";
+var texto1 = "Bienvenido al curso de JavaScript de curso Victor Robles";
 var texto2 = "es muy buen curso";
 
-//convierte cualquier dato a un string
-var dato = numero.toString(); 
-console.log(typeof dato);
-//convertir todos los caracteres en Mayusculas
-dato = texto1.toUpperCase();
-console.log(dato);
-//A minusculas
-dato = texto1.toLowerCase();
-console.log(dato);
+//Nos indica en que numero de caracter se encuentra la palabra que estamos buscando
+//podemos usar tambien search en lugar de indexOf
+var busqueda = texto1.indexOf('curso');
+console.log(busqueda);
 
-//Calcular longitud
-var nombre = "Marco Ruvalcaba";
-console.log(nombre.length);
+//Nos indica en que numero de caracter se encuentra la ultima coincidencia de la busqueda
+var busqueda = texto1.lastIndexOf('curso');
+console.log(busqueda);
 
-var arreglo = ["Ola","Hola"];
-console.log(arreglo.length);
+//Nos entrega la información en un array
+var busqueda = texto1.match('curso');
+console.log(busqueda);
 
-//Concatenar texto Unir
-var textoTotal = texto1+" "+texto2;
-console.log(textoTotal);
-//Usando la funcion concat para unir un texto
-var textoTotal = texto1.concat(" "+texto2);
-console.log(textoTotal);
+var busqueda = texto1.match(/curso/g);
+console.log(busqueda);
+
+//Extrae los caracteres que le indiquemos
+var busqueda = texto1.substr(23,10);
+console.log(busqueda);
+
+//Nos entrega la letra que se encuentra en la posicion que le indiquemos
+var busqueda = texto1.charAt(44);
+console.log(busqueda);
+
+//Nos indica true o false si los caracteres que indicamos estan al inicio de la cadena de texto
+var busqueda = texto1.startsWith('Bienvenido');
+console.log(busqueda);
+
+//Al final
+var busqueda = texto1.endsWith('Robles');
+console.log(busqueda);
+
+//En cualquier parte
+var busqueda = texto1.includes('curso');
+console.log(busqueda);
