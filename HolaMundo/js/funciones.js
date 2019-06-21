85901d97a3ccd7dd61721dc93497e3099a3c9844
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-20 19:01:31 -0500

    Funciones dentro de otras funciones

--- HolaMundo/js/funciones.js
+++ HolaMundo/js/funciones.js
@@ -2,24 +2,32 @@
 
 //parametros
 
+function porConsola(numero1,numero2){
+    console.log("Suma:           " + (numero1 + numero2));
+    console.log("Resta:          " + (numero1 - numero2));
+    console.log("Multiplicación: " + (numero1 * numero2));
+    console.log("División:       " + (numero1 / numero2));
+    console.log('**********************************');
+}
+
+function porPantalla(numero1,numero2){
+    document.write("Suma:           " + (numero1 + numero2) + '<br/>');
+    document.write("Resta:          " + (numero1 - numero2) + '<br/>');
+    document.write("Multiplicación: " + (numero1 * numero2) + '<br/>');
+    document.write("División:       " + (numero1 / numero2) + '<br/>');
+    document.write('**********************************<br/>');
+}
 
 function calculadora(numero1,numero2,mostrar = false){
-    
-    if (mostrar == false) {
-        console.log("Suma:           " + (numero1 + numero2));
-        console.log("Resta:          " + (numero1 - numero2));
-        console.log("Multiplicación: " + (numero1 * numero2));
-        console.log("División:       " + (numero1 / numero2));
-        console.log('**********************************');
+    let resultado = '';
+    if (mostrar == false){
+        porConsola(numero1,numero2);
     }else{
-        document.write("Suma:           "+(numero1+numero2)+'<br/>');
-        document.write("Resta:          "+(numero1-numero2)+'<br/>');
-        document.write("Multiplicación: "+(numero1*numero2)+'<br/>');
-        document.write("División:       "+(numero1/numero2)+'<br/>');
-        document.write('**********************************<br/>');
-        }
+        porPantalla(numero1,numero2);
+    }    
+    return true;
 }
 
 for (let i = 0; i <= 10; i++) {
-    calculadora(i,10,true); //Aqui agregamos el parametro opcional
+    calculadora(i,10,false); //Aqui agregamos el parametro opcional
 }
