cb46c4b0b896c46110f744e818ca354ca054e4ef
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-22 11:27:07 -0500

    Arrays básicos

---
+++ HolaMundo/js/arrays.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Arrays, Arreglos, Matices
+
+var nombre = "Marco Rafael";
+var nombres = ["Marco Rafael","Zuriel Ruvalcaba", "Violeta González", "Jose Martin",52,true];
+
+//como objeto
+var lenguajes = new Array("PHP","JS","Go","Java");
+
+ console.log(nombres);
+ console.log(lenguajes);console.log();
+
+ //Sacar un valor especifico del array
+ //Los indices inician desde el cero
+ console.log(nombres[2]);
