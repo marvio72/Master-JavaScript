f78f3aa49f6f49b53ac493654c9028ffd3a47aba
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-22 23:38:54 -0500

    Ordenar un Array

--- HolaMundo/js/arrays_multi.js
+++ HolaMundo/js/arrays_multi.js
@@ -2,20 +2,10 @@
 
 var categorias = ['Acción','Terror','Comedia','Romance'];
 
-var peliculas = ['La verdad duele','La vida es bella','Gran torino','Tiburon', 'El gran escape'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Zoolander', 'Gran torino', 'Tiburon', 'El gran escape'];
 
-var cine = [categorias,peliculas];
-
-// console.log(cine[0][1]);
-// console.log(cine[1][2]);
-
-
-// Convertir un array a un string
-var peliculas_string = peliculas.join();
-console.log(peliculas_string);
-
-//CONVERTIR UN TEXTO A UN ARRAY
-var cadena = "texto1,texto2,texto3";
-var cadena_array = cadena.split(',');
-console.log(cadena_array);
+//Ordenar un array
+console.log(peliculas.sort());
+//En reversa
+console.log(peliculas.reverse());
 
