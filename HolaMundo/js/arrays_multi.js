f85714b96d43b8c29113b6293a9723c1f809cc78
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-22 23:03:17 -0500

    Arrays multidimensionales

---
+++ HolaMundo/js/arrays_multi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var categorias = ['Acción','Terror','Comedia','Romance'];
+
+var peliculas = ['La verdad duele','La vida es bella','Gran torino'];
+
+var cine = [categorias,peliculas];
+
+console.log(cine[1][2]);
