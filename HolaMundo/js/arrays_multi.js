178a386180772e02ae1da683c5fe2c30eca5518e
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-22 23:34:22 -0500

    Convertir texto a un array y un array a texto

--- HolaMundo/js/arrays_multi.js
+++ HolaMundo/js/arrays_multi.js
@@ -2,38 +2,20 @@
 
 var categorias = ['Acción','Terror','Comedia','Romance'];
 
-var peliculas = ['La verdad duele','La vida es bella','Gran torino'];
+var peliculas = ['La verdad duele','La vida es bella','Gran torino','Tiburon', 'El gran escape'];
 
 var cine = [categorias,peliculas];
 
 // console.log(cine[0][1]);
 // console.log(cine[1][2]);
 
-/**************************
- * OPERACIONES CON ARRAYS *
- **************************/
-
-//Introducir datos a un array
-var agrega = "";
-
-do {
-    agrega = prompt("¿Qué pelicula deseas agregar?");
-    peliculas.push(agrega);
-} while (agrega != 'SALIR');
-    
-//ELIMINA EL ULTIMO REGISTRO DE UN ARRAY
-peliculas.pop();
-
-console.log(peliculas);
-
-//ELIMINAR UN REGISTRO SELECTIVAMENTE
-var indice = peliculas.indexOf('Gran torino');
-if (indice -1) {
-    peliculas.splice(indice,1);
-}
-console.log(peliculas);
 
 // Convertir un array a un string
 var peliculas_string = peliculas.join();
-
 console.log(peliculas_string);
+
+//CONVERTIR UN TEXTO A UN ARRAY
+var cadena = "texto1,texto2,texto3";
+var cadena_array = cadena.split(',');
+console.log(cadena_array);
+
