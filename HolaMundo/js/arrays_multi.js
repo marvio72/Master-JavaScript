0ff208f5d575e7ba64fb9e3f174a992e114c8afb
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-22 23:28:40 -0500

    Operaciones con Arrays

--- HolaMundo/js/arrays_multi.js
+++ HolaMundo/js/arrays_multi.js
@@ -6,4 +6,34 @@ var peliculas = ['La verdad duele','La vida es bella','Gran torino'];
 
 var cine = [categorias,peliculas];
 
-console.log(cine[1][2]);
+// console.log(cine[0][1]);
+// console.log(cine[1][2]);
+
+/**************************
+ * OPERACIONES CON ARRAYS *
+ **************************/
+
+//Introducir datos a un array
+var agrega = "";
+
+do {
+    agrega = prompt("¿Qué pelicula deseas agregar?");
+    peliculas.push(agrega);
+} while (agrega != 'SALIR');
+    
+//ELIMINA EL ULTIMO REGISTRO DE UN ARRAY
+peliculas.pop();
+
+console.log(peliculas);
+
+//ELIMINAR UN REGISTRO SELECTIVAMENTE
+var indice = peliculas.indexOf('Gran torino');
+if (indice -1) {
+    peliculas.splice(indice,1);
+}
+console.log(peliculas);
+
+// Convertir un array a un string
+var peliculas_string = peliculas.join();
+
+console.log(peliculas_string);
