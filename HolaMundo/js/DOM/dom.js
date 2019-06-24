5a34610d04bad6016648c15d35b87bf22c47c4a0
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-24 17:23:24 -0500

    Practicando con el DOM

--- HolaMundo/js/DOM/dom.js
+++ HolaMundo/js/DOM/dom.js
@@ -27,10 +27,40 @@ contenidoEnTexto.innerHTML = "He cambiado el texto";
 contenidoEnTexto.style.background = "green";
 contenidoEnTexto.style.padding = "15px";
 contenidoEnTexto.style.color = "white";
-console.log(contenidoEnTexto);
+
 //Conseguir elementos por su clase css
+
+var divRojo = document.getElementsByClassName('rojo');
+var div;
+//Por tratarse de un arreglo cuando seleccionamos de esta menera, para poder modificar cualquier elemento
+//de este array debemos realizar un bucle.
+for(div in divRojo){
+    if (divRojo[div].className == "rojo") {
+        divRojo[div].style.background = "red";
+    }
+}
+
+var divAmarillo = document.getElementsByClassName('amarillo');
+divAmarillo[0].style.background ="yellow";
+
+
 
 //Diferencias entre la funcion textContent VS innerHTML
 //innderHTML puede desplegar y manipular el texto
 //textContent solo puede mostrarnos el texto
 
+//Query Selector
+var id = document.querySelector("#encabezado");
+console.log(id);
+
+var claseRojo = document.querySelector(".rojo");
+console.log(claseRojo);
+
+var etiqueta = document.querySelector("div");
+console.log(etiqueta);
+
+//Cuando usamos querySelector, no nos entrega un array
+// como lo hace getElementsByClassName y todos sus derivados
+// Para seleccionar todos los elementos de una clase se
+//recomienda usar getElementsByClassName o en su caso
+//usar querySelectorAll
