f15b3a2c5f9cfcdbbd5453a905ad38fd588a64d0
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-25 17:44:30 -0500

    Validacion de formulario

--- HolaMundo/js/ejercicios/DOM/ejercicio-dom.js
+++ HolaMundo/js/ejercicios/DOM/ejercicio-dom.js
@@ -9,7 +9,41 @@ window.addEventListener('load', function(){
     formulario.addEventListener('submit', function(){
         var nombre = document.querySelector("#nombre").value;
         var apellido = document.querySelector("#apellido").value;
-        var edad = document.querySelector("#edad").value;
+        var edad = parseInt(document.querySelector("#edad").value);
+
+        function errores(campo){
+            var error = document.querySelector(campo);
+            error.style.background = "red";
+            error.style.padding = "2px";
+            error.style.color = "white";
+            error.style.fontSize = "14px";
+        }
+
+        if (nombre.trim() == null || nombre.trim().length == 0) {
+            document.querySelector("#error_nombre").innerHTML = "El nombre no es Válido<br>";
+            errores("#error_nombre");
+            return false;
+        }else{
+            document.querySelector("#error_nombre").style.display = "none";
+            
+        }
+        if (apellido.trim() == null || apellido.trim().length == 0) {
+            document.querySelector("#error_apellido").innerHTML = "El apellido no es Válido<br>";
+            errores("#error_apellido");
+            return false;
+        }else{
+            
+            document.querySelector("#error_apellido").style.display = "none";
+        }
+        if (isNaN(edad) || edad == null || edad <= 0) {
+            document.querySelector("#error_edad").innerHTML = "La edad no es válida<br>";
+            errores("#error_edad");
+            return false;
+        }else{
+            document.querySelector("#error_edad").style.display = "none";
+        }
+
+
     
         var datos_usuario = ["Nombre: "+nombre, "Apellido: "+apellido, "Edad: "+edad];
         var indice;
