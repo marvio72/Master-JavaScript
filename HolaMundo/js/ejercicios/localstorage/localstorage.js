86c1deb19d791ea4d30b7a913c7166b453da7603
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-29 17:53:01 -0500

    Ejercicio de localstorage

---
+++ HolaMundo/js/ejercicios/localstorage/localstorage.js
@@ -0,0 +1,36 @@
+'use strict';
+window.addEventListener('load', function(){
+
+     var formulario = document.querySelector("#formpeliculas");
+
+     formulario.addEventListener('submit',function(){
+        var titulo = document.querySelector("#addpelicula").value;
+        
+        if (titulo.length >= 1) {
+            localStorage.setItem(titulo, titulo);
+        }
+     });
+
+     
+
+     var ul = document.querySelector("#peliculas-list")
+     for (var i in localStorage) {
+         
+         if (typeof localStorage[i] == 'string') {
+             var li = document.createElement("li");
+             li.append(localStorage[i]);
+             ul.append(li);
+         }
+
+     }
+
+     var formularioB = document.querySelector("#formBorrar")
+     formularioB.addEventListener('submit', function () {
+         var titulob = document.querySelector("#delpelicula").value;
+        
+         if (titulob.length >= 1) {
+             localStorage.removeItem(titulob);
+         }
+         
+     });
+});
