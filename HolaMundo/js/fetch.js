1ca9384b509bb7a721d3b6316bc39fce047c185b
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-02 22:07:58 -0500

    fetch y Promesas

--- HolaMundo/js/fetch.js
+++ HolaMundo/js/fetch.js
@@ -5,23 +5,63 @@ window.addEventListener('load', function(){
     // Fetch (ajax) y peticiones a servicios / apis rest
 
     var div_usuarios = document.querySelector("#usuarios");
-    var usuarios = [];
+    var div_janet = document.querySelector("#janet");
+    
+    // var usuarios = [];
 
-    fetch('https://reqres.in/api/users')
-        .then(data => data.json())
-        .then(users => {
-            usuarios = users.data;
-            console.log(usuarios);
-       
+        getUsuarios()
+            .then(data => data.json())
+            .then(users => {
+                // usuarios = users.data;
+                listadoUsuarios(users.data);
+                
+                return getJanet();
+            })
+            .then(data => data.json())
+            .then(user => {
+                mostrarJanet(user.data);
+            });
 
+    function getUsuarios(){
+        return fetch('https://reqres.in/api/users');
+    }
+
+    function getJanet(){
+        return fetch('https://reqres.in/api/users/2');
+    }
+
+    function listadoUsuarios(usuarios) {
         usuarios.map((user, i) => {
-            let nombre = document.createElement("h3");
+          let nombre = document.createElement("h3");
+          let avatar = document.createElement("img");
 
-            nombre.innerHTML = i + '. ' + user.first_name + ' ' + user.last_name;
+          nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+          avatar.src = user.avatar;
+          avatar.width = '100';
+            
 
-            div_usuarios.appendChild(nombre);
+          div_usuarios.appendChild(nombre);
+          div_usuarios.appendChild(avatar);
 
-            document.querySelector(".loading").style.display = 'none';
+          document.querySelector(".loading").style.display = "none";
         });
-    });   
+    }
+    
+    function mostrarJanet(user) {
+        console.log(user);
+          let nombre = document.createElement("h4");
+          let avatar = document.createElement('img');
+
+          nombre.innerHTML = user.first_name + " " + user.last_name;
+          avatar.src = user.avatar;
+          avatar.width = '100';
+
+          div_janet.appendChild(nombre);
+          div_janet.appendChild(avatar);
+
+          document.querySelector("#janet .loading").style.display = "none";
+        
+    }
+
+    
 });
