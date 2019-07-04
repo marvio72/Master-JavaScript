df4d5b7618701d8d05c19fc6b57a7ada68afd620
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-03 19:53:56 -0500

    Capturar errores con tyr-catch

---
+++ HolaMundo/js/try-catch.js
@@ -0,0 +1,14 @@
+'use strict';
+window.addEventListener('load', function(){
+
+    try {
+        var year = 2019;
+        // alert(yea);
+        // console.log(decodeURIComponent("https://oversi%stemas.com"));
+        var vector = new Array(999999999999999999999999999999999999);
+    } catch (error) {
+        console.log(error);
+        this.alert("A ocurrido un error en el código")
+    }
+
+});
