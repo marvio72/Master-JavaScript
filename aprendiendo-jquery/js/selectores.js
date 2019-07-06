b278b870e595514ccde64525924584b57481717b
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-06 01:41:14 -0500

    Selector de id

---
+++ aprendiendo-jquery/js/selectores.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    'use strict';
+
+    $("#rojo").css("background", "red")
+        .css("color", "white");
+
+    $("#amarillo").css("background", "yellow")
+        .css("color", "green");
+
+    $("#verde").css("background", "green")
+        .css("color", "white");
+
+});
