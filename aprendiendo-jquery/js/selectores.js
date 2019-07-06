491324a09014e3b18c454d8cb65fb6821768b933
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-06 02:04:19 -0500

    Selector de Clases

--- aprendiendo-jquery/js/selectores.js
+++ aprendiendo-jquery/js/selectores.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     'use strict';
 
+    /*==============================================================================================
+    SELECTOR DE ID
+    ==============================================================================================*/
+
     $("#rojo").css("background", "red")
         .css("color", "white");
 
@@ -9,5 +13,25 @@ $(document).ready(function() {
 
     $("#verde").css("background", "green")
         .css("color", "white");
+
+     /*==============================================================================================
+     SELECTOR DE CLASES
+     ==============================================================================================*/
+
+    var mi_clase = $(".zebra");
+    //ver todos los elementos que tiene la clase .zebra
+    console.log(mi_clase);
+    // Nos muestra como un objeto uno de los elementos que contiene la clase .zebra
+    console.log(mi_clase.eq(1));
+    // Nos muestra el elemento de forma selectiva como si fuera un elemento de un array
+    console.log(mi_clase[0]);
+    
+    //Agregandole estilos.
+  
+    $(".sin_borde").click(function(){
+        console.log("Click dado!");
+        $(this).addClass("zebra");
+    });
+
 
 });
