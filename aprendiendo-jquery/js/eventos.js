8c469623a94e3f5634c32b81008b0fc6fd16eec1
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-07 14:04:42 -0500

    Evento Hover

--- aprendiendo-jquery/js/eventos.js
+++ aprendiendo-jquery/js/eventos.js
@@ -7,12 +7,35 @@ $(document).ready(function(){
     
     var caja = $("#caja");
 
-    caja.mouseover(function () { 
-        $(this).css("background","red");
-    });
+    // caja.mouseover(function () { 
+    //     $(this).css("background","red");
+    // });
 
-    caja.mouseout(function () { 
-        $(this).css("background","yellow");
-    });
+    // caja.mouseout(function () { 
+        // $(this).css("background", "yellow");
+    // });
 
+    /*==============================================================================================
+    HOVER
+    ==============================================================================================*/
+
+    // caja.hover(function () {
+    //         $(this).css("background","red");
+            
+    //     }, function () {
+    //         $(this).css("background", "yellow");
+    //     }
+    // );
+
+    //Otra manera de hacerlo
+    
+    function cambiaRojo() {
+        $(this).css("background", "red");
+    }
+
+    function cambiaVerde() {
+        $(this).css("background", "green");
+    }
+
+    caja.hover(cambiaRojo,cambiaVerde);
 });
