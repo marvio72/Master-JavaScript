d849e4cea033d7de38c46f220f26fa3278bd7c1b
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-08-08 19:36:29 -0500

    Agregar plugin de slider

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    'use strict';
+
+    
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200
+    });
+    
+
+});
