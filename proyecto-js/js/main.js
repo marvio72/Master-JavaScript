687889d19eec9028d27512d92a2cdd07c27d8ead
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-08-13 19:44:11 -0500

    Listado dinámico de artículos post

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,12 +1,62 @@
 $(document).ready(function(){
     'use strict';
 
-    
-    $('.bxslider').bxSlider({
+    // SLIDER
+    $('.galeria').bxSlider({
         mode: 'fade',
-        captions: true,
-        slideWidth: 1200
+        captions: false,
+        slideWidth: 1200,
+        responsive: true,
+        pager: true
     });
-    
+
+    // POSTS
+    var posts = [
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        },
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        },
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        },
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        },
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        },
+        {
+            title: 'Prueba de titulo',
+            date: 'Publicado el ' + moment().format("dddd D") + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore enim soluta qui hic, suscipit corrupti illum optio autem temporibus recusandae eius repellendus libero doloremque ab! Beatae doloribus a illo, consequatur non ipsa saepe earum fugit reprehenderit quia neque laboriosam harum cum veritatis doloremque quod dignissimos, vel architecto nulla maiores, ex nemo laudantium odio magni. Quidem doloremque veniam corporis nostrum voluptas.'
+        }
+    ];
+
+    posts.forEach((item,index) => {
+        var post = `
+            <article class="post">
+            <h2>${item.title} ${index+1}</h2>
+            <span class="date">${item.date}</span>
+            <p>${item.content}</p>
+            <a href="#" class="button-more">
+                Leer más
+            </a>
+            </article>`;
+
+            $("#posts").append(post);
+    });
 
 });
+    
