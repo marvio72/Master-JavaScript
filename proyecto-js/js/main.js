f5bf840373485fcceeeeaf71d34b24a63e4b389f
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-05 13:27:24 -0500

    Identificacion con localStorage

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -84,5 +84,43 @@ $(document).ready(function(){
 
     $(window).on("load",tema);
 
+
+     /*==============================================================================================
+     SCROLL ARRIBA DE LA WEB
+     ==============================================================================================*/
+     $('.subir').click(function (e) { 
+         e.preventDefault();
+         
+         $("html, body").animate({
+             scrollTop: 0
+         }, 500);
+     });
+
+     /*==============================================================================================
+     LOGIN FALSO
+     ==============================================================================================*/
+     $("#login form").submit(function (e) { 
+         e.preventDefault();
+         var nombre = $("#name").val();
+         localStorage.setItem("nombre", nombre);
+         location.reload();
+     });
+
+     var form_name = localStorage.getItem("nombre");
+
+     if (form_name != null && form_name != "undefined") {
+         var about_parrafo = $("#about p");
+
+         about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong><br>");
+         about_parrafo.append('<a href="#" id="logout">Cerrar Sesión</a>');
+
+         $("#login").hide();
+         $("#logout").click(function (e) { 
+             e.preventDefault();
+             localStorage.removeItem("nombre");
+             location.reload();
+         });
+     }
+
 });
     
