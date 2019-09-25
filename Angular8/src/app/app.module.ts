e6c04588d2d9c4768ccc63adcf5b1eff6b78f531
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-25 18:51:47 -0500

    Multiples componentes

--- Angular8/src/app/app.module.ts
+++ Angular8/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VideoJuegoComponent } from './videojuego/videojuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoJuegoComponent
+    VideoJuegoComponent,
+    ZapatillasComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular8/src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zapatillas',
+  templateUrl: './zapatillas.component.html'
+})
+
+export class ZapatillasComponent {
+
+  public titulo: string = 'Zapatillas de temporada';
+
+
+}
