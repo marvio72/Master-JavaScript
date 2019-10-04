a24a3cfcb52722b990439286b45ad4f551d3260c
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-04 13:10:39 -0500

    Generar componentes con la consola

--- Angular8/src/app/app.component.ts
+++ Angular8/src/app/app.component.ts
@@ -1,10 +1,10 @@
  import { Component } from '@angular/core';
 
-@Component({
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular8';
+  titulo = 'Angular8';
 }
--- Angular8/src/app/app.module.ts
+++ Angular8/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VideoJuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoJuegoComponent,
-    ZapatillasComponent
+    ZapatillasComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
