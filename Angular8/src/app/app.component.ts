4c41de83dd45c625112f4a235e0c8929434a9589
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-11 16:37:11 -0500

    Importar y Exporta

--- Angular8/src/app/app.component.ts
+++ Angular8/src/app/app.component.ts
@@ -1,4 +1,5 @@
  import { Component } from '@angular/core';
+ import { Configuracion } from './models/configuracion';
 
  @Component({
   selector: 'app-root',
@@ -8,6 +9,14 @@
 export class AppComponent {
   public titulo = 'Angular8';
   public mostrarVideojuegos: boolean = true;
+  public descripcion: string;
+  public config;
+
+  constructor() {
+    this.titulo = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+    this.config = Configuracion;
+  }
 
   ocultarVideojuegos(value) {
     this.mostrarVideojuegos = value;
---
+++ Angular8/src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export let Configuracion = {
+  color: "#cccccc",
+  fondo: "#101070",
+  titulo: "Bienvenido al Master en JavaScript y Angular",
+  descripcion: "Aprendiendo Angular con Víctor Robles"
+};
