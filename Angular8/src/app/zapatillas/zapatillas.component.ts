236c597e10b8381442d16fae5ec3fbdf76662abb
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-11 18:38:20 -0500

    Directiva  bucle ngFor

--- Angular8/src/app/zapatillas/zapatillas.component.ts
+++ Angular8/src/app/zapatillas/zapatillas.component.ts
@@ -10,18 +10,35 @@ export class ZapatillasComponent implements OnInit {
 
   public titulo: string = 'Zapatillas de temporada';
   public zapatillas: Array<Zapatilla>;
+  // tslint:disable-next-line: ban-types
+  public marcas: String[];
 
   constructor() {
+    this.marcas = new Array();
     this.zapatillas = [
       new Zapatilla('Rebook Classic', 'Rebook', 'Blanco', 80, true),
       new Zapatilla('Aero', 'Nike', 'Negro', 87, true),
       new Zapatilla('Samba', 'Adidas', 'Negro', 129, true),
-      new Zapatilla('Durango', 'Guess', 'Gris', 67, false)
+      new Zapatilla('Durango', 'Nike', 'Gris', 67, false)
     ];
   }
 
   ngOnInit(): void {
     console.log(this.zapatillas);
+
+    this.getMarcas();
+  }
+
+  getMarcas() {
+    this.zapatillas.forEach((zapatilla, index) => {
+      if (this.marcas.indexOf(zapatilla.marca) < 0) {
+
+        this.marcas.push(zapatilla.marca);
+
+      }
+    });
+
+    console.log(this.marcas);
   }
 
 }
