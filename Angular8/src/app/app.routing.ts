55b60fc5b52853ec59ba9c7c2b514b9355b7a390
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-29 19:19:15 -0600

    Parametros por la URL

--- Angular8/src/app/app.routing.ts
+++ Angular8/src/app/app.routing.ts
@@ -15,6 +15,7 @@ const appRoutes: Routes = [
   {path: 'zapatillas', component: ZapatillasComponent},
   {path: 'videojuego', component: VideoJuegoComponent},
   {path: 'cursos', component: CursosComponent},
+  {path: 'cursos/:nombre/:followers', component: CursosComponent},
   {path: '**', component: HomeComponent}
 ];
 
--- Angular8/src/app/cursos/cursos.component.ts
+++ Angular8/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-cursos',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  public nombre: string;
+  public followers: number;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this._route.params.subscribe((params: Params) => {
+      this.nombre = params.nombre;
+      this.followers = +params.followers;
+    });
   }
 
 }
