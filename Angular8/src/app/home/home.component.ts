6404c025c3574a0a5863e22cfb0e1793b81b47fa
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-31 13:52:01 -0600

    ngTemplate  If y else en las vistas

--- Angular8/src/app/home/home.component.ts
+++ Angular8/src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public identificado: boolean;
 
-  constructor() { }
+  constructor() {
+    this.identificado = false;
+   }
 
   ngOnInit() {
+  }
+
+  setIdentificado() {
+    this.identificado = true;
+  }
+
+  unsetIdentificado() {
+    this.identificado = false;
   }
 
 }
