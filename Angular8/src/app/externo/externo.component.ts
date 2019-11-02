6c0660755ea2ec34ad692feaa378b9b7d0dd4198
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-11-01 23:56:53 -0600

    Formularios, AJAX y HTTP Post

--- Angular8/src/app/externo/externo.component.ts
+++ Angular8/src/app/externo/externo.component.ts
@@ -13,11 +13,17 @@ export class ExternoComponent implements OnInit {
     public user: any;
     public userId: any;
     public fecha: any;
+    public new_user: any;
+    public usuario_guardado: any;
 
   constructor(
     private _peticionesService: PeticionesService
   ) {
     this.userId = 1;
+    this.new_user = {
+      'name': '',
+      'job': ''
+    };
   }
 
   ngOnInit() {
@@ -30,6 +36,19 @@ export class ExternoComponent implements OnInit {
     this._peticionesService.getUser(this.userId).subscribe(
       result => {
         this.user = result.data;
+      },
+      error => {
+        console.log(error as any);
+      }
+    );
+  }
+
+  onSubmit(form) {
+    this._peticionesService.addUser(this.new_user).subscribe(
+      response => {
+        this.usuario_guardado = response;
+
+        form.reset();
       },
       error => {
         console.log(error as any);
--- Angular8/src/app/services/peticiones.service.ts
+++ Angular8/src/app/services/peticiones.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Params } from '@angular/router';
+import { __param } from 'tslib';
 
 @Injectable()
 export class PeticionesService {
@@ -13,5 +15,12 @@ export class PeticionesService {
 
   getUser(userId): Observable<any> {
     return this._http.get(this.url + '/api/users/' + userId);
+  }
+
+  addUser(user): Observable<any> {
+    const params = JSON.stringify(user);
+    const cabecera = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this._http.post(this.url + '/api/users', params, {headers: cabecera});
   }
 }
