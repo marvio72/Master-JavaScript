f7d8fd22edd7837dec703331891c75b934e20975
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-11-19 18:59:53 -0600

    Listar  proyectos del portafolio

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -43,7 +43,27 @@ var controller = {
         project: projectStored
       });
 
-    }); 
+    });
+  },
+
+  //solicitar información de la base de datos a través del id
+  getProject: function(req, res){
+    var projectId = req.params.id;
+
+    if (projectId == null) return res.status(404).send({
+      message: 'El proyecto no existe'
+    });
+    
+    Project.findById(projectId,(err, project) =>{
+
+      if(err) return res.status(500).send({message: 'Error al devolver los datos'});
+
+      if(!project) return res.status(404).send({message: 'El proyecto no existe'});
+
+      return res.status(200).send({
+        project
+      });
+    });
   }
 };
 
--- backend/rutes/project.js
+++ backend/rutes/project.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.get('/home', ProjectController.home);
 router.post('/test', ProjectController.test);
 router.post('/save-project', ProjectController.saveProject);
+// El ? indica que el parametro es opcional
+router.get('/project/:id?', ProjectController.getProject);
 
 module.exports = router;
